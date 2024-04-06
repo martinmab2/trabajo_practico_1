90b6bd71078298e99c0e3d11c7a5034cf9691c60
Martin Alejandro Bonari <131064127+martinmab2@users.noreply.github.com>
2024-04-06 00:51:53 -0300

    Ejercicio 2 finalizado

---
+++ Tp1_BonariMartinAlejandro/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,17 @@
+package ar.edu.unju.fi.ejercicio2;
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String nombrePais="Suiza.";
+		int edad=34;
+		double alturaEdificio =354.03;
+		double precioProducto =39.99;
+		String numeroTelefonico ="3874064403";
+		double coseno = Math.cos(0.5);
+		
+		System.out.println("Nombre de pais: " + nombrePais + "     Edad de una persona: " + edad + "      Altura de un edificio: " + alturaEdificio + "m        Precio de un producto: " + precioProducto + "$        Numero telefonico: " + numeroTelefonico + "         Coseno de 0,5: " + coseno);
+
+	}
+
+}
