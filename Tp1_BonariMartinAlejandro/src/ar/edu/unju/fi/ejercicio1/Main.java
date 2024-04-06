b610c2f2f4f297295ef5e2f3b0970b58208d4587
Martin Alejandro Bonari <131064127+martinmab2@users.noreply.github.com>
2024-04-06 00:00:31 -0300

    Ejercicio uno finalizado

--- Tp1_BonariMartinAlejandro/src/ar/edu/unju/fi/ejercicio1/Main.java
+++ Tp1_BonariMartinAlejandro/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -3,7 +3,19 @@ package ar.edu.unju.fi.ejercicio1;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// Declaración de variables para almacenar valores numéricos
+		int n1 = 8;
+		int n2 = 5;
+		int n3 = 1;
+		int n4 = 20;
+		int n5 = 9;
+
+		// Cálculo del promedio
+		double prom = (n1 + n2 + n3 + n4 + n5) / 5.0;
+
+		// Mostrar el resultado por consola
+		System.out.println("Promedio : " + prom);
 
 	}
 
