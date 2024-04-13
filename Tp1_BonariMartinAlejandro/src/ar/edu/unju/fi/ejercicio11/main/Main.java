2dd8322b41136fb57e1e16babfef8cc923dae33c
Martin Alejandro Bonari <131064127+martinmab2@users.noreply.github.com>
2024-04-13 04:22:49 -0300

    Asistencia 12/4 (Ejercicio11)

--- Tp1_BonariMartinAlejandro/src/ar/edu/unju/fi/ejercicio11/main/Main.java
+++ Tp1_BonariMartinAlejandro/src/ar/edu/unju/fi/ejercicio11/main/Main.java
@@ -1,5 +1,5 @@
 package ar.edu.unju.fi.ejercicio11.main;
-
+//Ejercicio11
 public class Main {
 	public static void main(String[] args) {
 		int j = 40;
@@ -9,6 +9,9 @@ public class Main {
 			resultado = j * i;
 			System.out.print(" " + resultado);
 			i++;
+			// Resultado esperado:80 117 152 185 216 245 272 297 320 341 360 377 392 405 416
+			// 425 432 437 440 441 440 437 432 425 416 405 392 377 360 341 320 297 272 245
+			// 216 185 152 117 80 41
 		}
 	}
 }
